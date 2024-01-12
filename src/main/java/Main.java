f8b684662fd5de636d8ab0a60ba25ac2ac0dd810
little_grees <735834365@qq.com>
2024-01-12 22:09:10 +0800

    pass 2st stage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,9 @@
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 
 public class Main {
   public static void main(String[] args) {
@@ -12,6 +15,10 @@ public class Main {
       serverSocket.setReuseAddress(true);
       clientSocket = serverSocket.accept(); // Wait for connection from client.
       System.out.println("accepted new connection");
+      OutputStream outputStream = clientSocket.getOutputStream();
+      String response = "HTTP/1.1 200 OK\r\n\r\n";
+      outputStream.write(response.getBytes(StandardCharsets.UTF_8));
+      outputStream.flush();
     } catch (IOException e) {
       System.out.println("IOException: " +e.getMessage());
     }
