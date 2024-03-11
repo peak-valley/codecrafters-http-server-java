df016be8fff57a62eb7d0b785aca74e96d3c9b4f
little_grees <735834365@qq.com>
2024-03-11 17:59:14 +0800

    stage 3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,26 +1,40 @@
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import com.zyf.constant.Constants;
+
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
+import java.util.List;
 
-public class Main {
-  public static void main(String[] args) {
+    public class Main {
+    private static List<String> paths = List.of("/");
+    public static void main(String[] args) {
     ServerSocket serverSocket = null;
     Socket clientSocket = null;
 
     try {
-      serverSocket = new ServerSocket(4221);
-      serverSocket.setReuseAddress(true);
-      clientSocket = serverSocket.accept(); // Wait for connection from client.
-      System.out.println("accepted new connection");
-      OutputStream outputStream = clientSocket.getOutputStream();
-      String response = "HTTP/1.1 200 OK\r\n\r\n";
-      outputStream.write(response.getBytes(StandardCharsets.UTF_8));
-      outputStream.flush();
+        serverSocket = new ServerSocket(4221);
+        serverSocket.setReuseAddress(true);
+        clientSocket = serverSocket.accept(); // Wait for connection from client.
+        System.out.println("accepted new connection");
+
+        OutputStream outputStream = clientSocket.getOutputStream();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+        String line = reader.readLine();
+        String[] chunk = line.split(" ");
+        String path = chunk[1];
+        String response;
+        if (paths.contains(path)) {
+            response = Constants.OK;
+      } else {
+            response = Constants.NOT_FOUND;
+        }
+
+        outputStream.write(response.getBytes(StandardCharsets.UTF_8));
+        outputStream.flush();
     } catch (IOException e) {
-      System.out.println("IOException: " +e.getMessage());
+        System.out.println("IOException: " +e.getMessage());
     }
-  }
+    }
 }
