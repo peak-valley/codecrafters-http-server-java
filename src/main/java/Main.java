a086b1cee5851e2f36268ee6a3e9ebc54c0c30de
little_grees <735834365@qq.com>
2024-03-13 17:07:54 +0800

    stage 3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
 import com.zyf.constant.Constants;
+import lombok.extern.slf4j.Slf4j;
 
 import java.io.*;
 import java.net.ServerSocket;
@@ -6,37 +7,44 @@ import java.net.Socket;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
 
-    public class Main {
+@Slf4j
+public class Main {
     private static List<String> paths = List.of("/");
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         ServerSocket serverSocket = null;
         Socket clientSocket = null;
 
-        try {
-            serverSocket = new ServerSocket(4221);
-            serverSocket.setReuseAddress(true);
-            clientSocket = serverSocket.accept(); // Wait for connection from client.
-            System.out.println("accepted new connection");
+        serverSocket = new ServerSocket(4221);
+        serverSocket.setReuseAddress(true);
+        clientSocket = serverSocket.accept(); // Wait for connection from client.
+        System.out.println("accepted new connection");
 
-            OutputStream outputStream = clientSocket.getOutputStream();
-            InputStream inputStream = clientSocket.getInputStream();
+        OutputStream outputStream = clientSocket.getOutputStream();
+        InputStream inputStream = clientSocket.getInputStream();
 
-            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+        String line;
+        String method;
+        String path;
+        String httpVersion;
+        String response;
+        for (int i = 0; i < 3; i++) {
+            line = reader.readLine();
+            log.info("read line:{}", line);
+            if (i == 0) {
+                String[] chunk = line.split(" ");
+                path = chunk[1];
+                if (paths.contains(path)) {
+                    response = Constants.OK;
+                } else {
+                    response = Constants.NOT_FOUND;
+                }
+                outputStream.write(response.getBytes(StandardCharsets.UTF_8));
+                outputStream.flush();
+            } else if (i == 1) {
+            } else if (i == 2) {
 
-            String line = reader.readLine();
-            String[] chunk = line.split(" ");
-            String path = chunk[1];
-            String response;
-            if (paths.contains(path)) {
-                response = Constants.OK;
-          } else {
-                response = Constants.NOT_FOUND;
             }
-
-            outputStream.write(response.getBytes(StandardCharsets.UTF_8));
-            outputStream.flush();
-        } catch (IOException e) {
-            System.out.println("IOException: " +e.getMessage());
         }
     }
 }
